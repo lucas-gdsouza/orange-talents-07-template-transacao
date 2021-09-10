898c0cfc4e4269d5cc19c771824da0ea21d22d57
Lucas Souza <lucas.souza@zup.com.br>
2021-09-10 18:45:54 -0300

    autenticacao_autorizacao

---
+++ src/main/java/br/com/zupacademy/transacao/security/ResourceServerConfig.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.transacao.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/api/v1/transacoes/**").hasAuthority("SCOPE_escopo-transacoes")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
